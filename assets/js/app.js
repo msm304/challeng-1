0742f30a45bdb9d998d93d967e6514577758a23f
msm304 <soltani5747@gmail.com>
2023-02-24 11:09:19 +0330

    responsive filter for max-width:769px

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// hamburger menu
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
